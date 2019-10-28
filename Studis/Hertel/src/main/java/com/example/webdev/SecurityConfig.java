1368cbf7e3aa64610e331b89d77ed5d441896eee
Willi <Willi.Hertel@web.de>
2019-10-28 22:40:09 +0100

    Basic Setup

---
+++ Studis/Hertel/src/main/java/com/example/webdev/SecurityConfig.java
@@ -0,0 +1,63 @@
+package com.example.webdev;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.WebUtils;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http
+                .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .and()
+                .addFilterAfter(new CsrfHeaderFilter(), CsrfFilter.class)
+                .csrf()
+                .csrfTokenRepository(csrfTokenRepository());
+    }
+
+
+    private CsrfTokenRepository csrfTokenRepository() {
+        HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
+        repository.setHeaderName("X-XSRF-TOKEN");
+        return repository;
+    }
+
+
+    private class CsrfHeaderFilter extends OncePerRequestFilter {
+        @Override
+        protected void doFilterInternal(HttpServletRequest request,
+                                        HttpServletResponse response, FilterChain filterChain)
+                throws ServletException, IOException {
+            CsrfToken csrf = (CsrfToken) request.getAttribute(CsrfToken.class
+                    .getName());
+            if (csrf != null) {
+                Cookie cookie = WebUtils.getCookie(request, "XSRF-TOKEN");
+                String token = csrf.getToken();
+                if (cookie==null || token!=null && !token.equals(cookie.getValue())) {
+                    cookie = new Cookie("XSRF-TOKEN", token);
+                    cookie.setPath("/");
+                    response.addCookie(cookie);
+                }
+            }
+            filterChain.doFilter(request, response);
+        }
+    }
+}
