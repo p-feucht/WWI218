8881f81d45c1cd3a5773d44b30f41baf754ec180
p-feucht <p-feucht@web.de>
2019-11-30 09:51:08 +0100

    added example for database entry

---
+++ Vorlesung/Beispiele/form.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+
+<form action="mysql-connect.php" method="post">
+Vorname: <input type="text" name="vorname"><br>
+Nachname: <input type="text" name="nachname"><br>
+E-mail: <input type="text" name="email"><br>
+Nachricht: <input type="text" name="nachricht"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
